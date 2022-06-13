f941d1009ac0d0501e359979b36f8bb7f1d534e7
shamao3 <yicong996873@163.com>
2022-06-13 10:51:51 +0800

    modify log output of gcn_0

--- src/py/flwr_example/gcn_0/client2.py
+++ src/py/flwr_example/gcn_0/client2.py
@@ -192,8 +192,9 @@ if __name__ == "__main__":
 
     if not os.path.exists('log/'):
         os.mkdir('log/')
-    with open(f'log/client2_{args.n}.log', mode='w', encoding='utf-8') as f:
+    with open(f'log/client0_{args.n}.log', mode='w', encoding='utf-8') as f:
         for i in range(len(log_info)):
             item = log_info[i]
-            f.write(str(i) + "," + str(item[0]) + "," + str(item[1]) + '\n')
+            f.write(" (" + str(i) + "," + str(item[0]) + "," + str(item[1]) + "),")
+        f.write("]")
     print(f'training completed')
