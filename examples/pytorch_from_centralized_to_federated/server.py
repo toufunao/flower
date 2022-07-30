a592e816335f682f66744df314cc2ed33597e46d
wurj <wurjpersonal@126.com>
2022-07-30 13:51:24 +0800

    feat: rm unused files and keep proj codes

--- examples/pytorch_from_centralized_to_federated/server.py
+++
@@ -1,7 +0,0 @@
-"""Flower server example."""
-
-
-import flwr as fl
-
-if __name__ == "__main__":
-    fl.server.start_server("[::]:8080", config={"num_rounds": 3})
--- examples/quickstart_mxnet/server.py
+++
@@ -1,6 +0,0 @@
-"""Flower server example."""
-
-import flwr as fl
-
-if __name__ == "__main__":
-    fl.server.start_server("0.0.0.0:8080", config={"num_rounds": 3})
